07148d1f8aafc07c82c87ca2a5f10f6f68ab4c21
imChenJian <imchenjian@imChenJiandeMacBook-Pro.local>
2016-03-21 16:54:18 +0800

    fix pendingval

--- test/testpjs.js
+++ test/testpjs.js
@@ -13,15 +13,18 @@ setTimeout(function(){
 },1500)
 
 
+
 pjs.when(["A", "B"], function(valMap){
 	/*把A和B的返回值传到回调函数里面*/
 	console.log(valMap)
 	console.log("A-B-Done!")
 })
 
+
 pjs.when(["A", "C"], function(valMap){
 	console.log(valMap)
 	console.log("A-C-Done!")
 })
+
 
 //setTimeout可以是ajax等异步操作
